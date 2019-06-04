4cb8aa44c147ffebb880d3435d35f87d2cdf3ed6
ndreamer92 <31407184+ndreamer92@users.noreply.github.com>
2019-06-04 16:49:18 +0300

    New homepage lists

---
+++ src/components/GameTopList.vue
@@ -0,0 +1,55 @@
+<template>
+    <v-flex >
+      <v-card>
+        <v-list subheader>
+          <v-subheader>{{headerText}}</v-subheader>
+          <v-list-tile
+            v-for="game in games"
+            :key="game.title"
+            avatar
+            @click="navigateToGame(game.id)"
+          >
+            <v-list-tile-avatar>
+              <img :src="game.header_image">
+            </v-list-tile-avatar>
+
+            <v-list-tile-content>
+              <v-list-tile-title v-html="game.title"></v-list-tile-title>
+            </v-list-tile-content>
+
+            <v-list-tile-action>
+              <v-icon :color="game.active ? 'teal' : 'grey'">add_shopping_cart</v-icon>
+            </v-list-tile-action>
+          </v-list-tile>
+        </v-list>
+      </v-card>
+    </v-flex>
+
+</template>
+
+<script>
+export default {
+  props: {
+    games: {
+      type: Object,
+      required: true
+    },
+    headerText: {
+      type: String,
+      required: false
+    }
+  },
+  data () {
+      return {
+      }
+    },
+  methods: {
+    navigateToGame(gameId){
+      this.$router.push({ name: "game", params: { gameId: gameId } });
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,57 +1,76 @@
 <template>
   <v-container>
-    
     <v-layout align-center justify-center column fill-height>
       <v-flex xs4 offset-xs-4>
         <h1>Добро пожаловать</h1>
       </v-flex>
     </v-layout>
     <v-layout align-center justify-center column fill-height>
-      <v-flex >
+      <v-flex>
         <news-block></news-block>
       </v-flex>
     </v-layout>
-    <v-layout align-center justify-center column fill-height>
-      <v-flex xs4 offset-xs-4 pa-4>
-        <h1>Популярные предложения</h1>
-      </v-flex>
-    </v-layout>
     <v-layout align-center justify-space-around row fill-height>
-      <v-flex v-for="game in topGames" :key="game.id" xs2 >
-        <game-card-mini :game="game"></game-card-mini>
-      </v-flex>
-    </v-layout>
-    <v-layout align-center justify-center column fill-height>
-      <v-flex xs4 offset-xs-4 pa-4>
-        <h1>Новые поступления</h1>
-      </v-flex>
-    </v-layout>
-    <v-layout align-center justify-space-around row fill-height>
-      <v-flex v-for="game in recentAdded" :key="game.id" xs2 >
-        <game-card-mini :game="game"></game-card-mini>
-      </v-flex>
-    </v-layout>
-    <v-layout align-center justify-center column fill-height>
-      <v-flex xs4 offset-xs-4 pa-4>
-        <h1>Скидки</h1>
-      </v-flex>
-    </v-layout>
-    <v-layout align-center justify-space-around row fill-height>
-      <!-- <v-flex xs2>
-        <game-card-mini :game="1"></game-card-mini>
-      </v-flex>
-      <v-flex xs2>
-        <game-card-mini :game="1"></game-card-mini>
-      </v-flex>
-      <v-flex xs2>
-        <game-card-mini :game="1"></game-card-mini>
-      </v-flex>
-      <v-flex xs2>
-        <game-card-mini :game="1"></game-card-mini>
-      </v-flex>
-      <v-flex xs2>
-        <game-card-mini :game="1"></game-card-mini>
-      </v-flex> -->
+      <v-tabs centered color="amber " dark icons-and-text>
+        <v-tabs-slider color="black"></v-tabs-slider>
+
+        <v-tab href="#tab-1">
+          PC
+          <!-- <v-icon>phone</v-icon> -->
+        </v-tab>
+
+        <v-tab href="#tab-2">
+          PS4
+          <!-- <v-icon>favorite</v-icon> -->
+        </v-tab>
+
+        <v-tab href="#tab-3">
+          Other
+          <!-- <v-icon>account_box</v-icon> -->
+        </v-tab>
+
+        <v-tab-item :value="'tab-1'">
+          <v-layout align-center justify-space-around row fill-height>
+            <span>
+              <game-top-list :games="topGames" :headerText="'Popular Games'"></game-top-list>
+            </span>
+            <span>
+              <game-top-list :games="recentAdded" :headerText="'Recently Added'"></game-top-list>
+            </span>
+            <span>
+              <game-top-list :games="topGames" :headerText="'Yet another top'"></game-top-list>
+            </span>
+          </v-layout>
+        </v-tab-item>
+
+        <v-tab-item :value="'tab-2'">
+          <v-layout align-center justify-space-around row fill-height>
+            <span>
+              <game-top-list :games="topGames" :headerText="'Popular Games'"></game-top-list>
+            </span>
+            <span>
+              <game-top-list :games="recentAdded" :headerText="'Recently Added'"></game-top-list>
+            </span>
+            <span>
+              <game-top-list :games="topGames" :headerText="'Yet another top'"></game-top-list>
+            </span>
+          </v-layout>
+        </v-tab-item>
+
+        <v-tab-item :value="'tab-3'">
+          <v-layout align-center justify-space-around row fill-height>
+            <span>
+              <game-top-list :games="topGames" :headerText="'Popular Games'"></game-top-list>
+            </span>
+            <span>
+              <game-top-list :games="recentAdded" :headerText="'Recently Added'"></game-top-list>
+            </span>
+            <span>
+              <game-top-list :games="topGames" :headerText="'Yet another top'"></game-top-list>
+            </span>
+          </v-layout>
+        </v-tab-item>
+      </v-tabs>
     </v-layout>
   </v-container>
 </template>
@@ -59,40 +78,41 @@
 
 
 <script>
-import GameCardMini from "../components/GameCardMini";
 import NewsBlock from "../components/NewsBlock";
+import GameTopList from "../components/GameTopList";
 
 export default {
   data() {
     return {
-      
+      text:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
     };
   },
-  created(){
+  created() {
     //this.topGames = this.$store.getters.getTopGames;
   },
   computed: {
-    topGames: function (){
+    topGames: function() {
       var temp = this.$store.getters.getTopGames;
       var result;
-      if (temp){
-        result = temp.slice(0,5);
+      if (temp) {
+        result = temp.slice(0, 5);
       }
       return result;
     },
-    recentAdded: function (){
+    recentAdded: function() {
       var temp = this.$store.getters.getRecentAddedGames;
       var result;
-      if (temp){
-        result = temp.slice(0,5);
+      if (temp) {
+        result = temp.slice(0, 5);
       }
       return result;
     }
   },
 
   components: {
-    GameCardMini,
-    NewsBlock
+    NewsBlock,
+    GameTopList
   }
 };
 </script>
