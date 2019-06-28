7e3e4c7091779c504e5d45fd0831cd410bac4d9a
ndreamer92 <31407184+ndreamer92@users.noreply.github.com>
2019-06-28 13:41:39 +0300

    Dev
    Adaptive layout for game details (part 1)

--- src/components/GameDetails.vue
+++ src/components/GameDetails.vue
@@ -1,11 +1,10 @@
 <template>
   <v-container fluid>
-    <div v-if="game">
+    <div v-if="game && !$vuetify.breakpoint.xs">
       <v-layout pa-2>
         <h1>{{game.title}}</h1>
-        <!-- <h1 v-if="game.is_dlc">DLC</h1> -->
       </v-layout>
-      <!-- Primary image for game, basic info -->
+      <!-- Primary image for game, basic info $vuetify.breakpoint.mdAndDown -->
       <v-layout align-start justify-start row fill-height>
         <v-flex xs4 pa-2>
           <v-img
@@ -39,14 +38,13 @@
         <v-spacer></v-spacer>
       </v-layout>
       <v-layout xs12 pa-3>
-        
         <h5>{{game.short_description}}</h5>
       </v-layout>
       <v-divider></v-divider>
       <!-- Screen gallery -->
       <h5>Скриншоты:</h5>
       <v-layout align-start justify-space-between row>
-        <v-flex xs12>
+        <v-flex xs8>
           <gallery :images="images" :index="index" @close="index = null"></gallery>
           <v-flex
             class="image"
@@ -58,6 +56,67 @@
           ></v-flex>
         </v-flex>
       </v-layout>
+    </div>
+    <!-- adaptive mobile version -->
+    <div v-if="game && $vuetify.breakpoint.xs">
+      <v-layout align-center justify-center pa-2>
+        <h2>{{game.title}}</h2>
+      </v-layout>
+      <v-layout align-start justify-start row fill-height pa-2>
+        <v-flex xs6>
+          <v-img
+            class="white--text"
+            height="150px"
+            aspect-ratio="2"
+            contain
+            :src="game.header_image"
+          ></v-img>
+        </v-flex>
+        <v-flex xs6>
+          <metacriticsScore :game="game"></metacriticsScore>
+        </v-flex>
+      </v-layout>
+      <v-layout align-center justify-center>
+        <v-flex xs6>
+          <h5>Жанры:</h5>
+          <p>{{game.genres}}</p>
+        </v-flex>
+        <v-flex xs6>
+          <h5>Языки:</h5>
+          <p>{{game.supported_languages}} English, Russian</p>
+        </v-flex>
+      </v-layout>
+      <v-layout align-center justify-center>
+        <v-flex xs6>
+          <h5>Разработчик:</h5>
+          <p>{{game.developers}}</p>
+        </v-flex>
+        <v-flex xs6>
+          <h5>Дата выхода:</h5>
+          <p>{{game.release_date}} 01.01.1970</p>
+        </v-flex>
+      </v-layout>
+      <v-layout align-center justify-center>
+        <v-flex>
+          <h4>Описание:</h4>
+          <p>{{game.short_description}}</p>
+        </v-flex>
+      </v-layout>
+      <h5>Скриншоты:</h5>
+      <v-layout align-start justify-space-between row>
+        <v-flex xs12>
+          <gallery :images="images" :index="index" @close="index = null"></gallery>
+          <v-flex
+            class="image"
+            v-for="(image, imageIndex) in screenshots"
+            :key="imageIndex"
+            @click="index = imageIndex"
+            :style="{ backgroundImage: 'url(' + image + ')', width: '130px', height: '100px' }"
+            aspect-ratio="0.5"
+          ></v-flex>
+        </v-flex>
+      </v-layout>
+      <steam-price :game="game"></steam-price>
     </div>
   </v-container>
 </template>
